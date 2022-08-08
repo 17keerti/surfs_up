e7a003c4f83b41a6ecbd781db0b010d46225e247
Keerti Chaudhary <17keerti@gmail.com>
2022-08-08 00:31:13 -0700

    Route to report on the min, avg, and max temperature

--- app.py
+++ app.py
@@ -68,5 +68,24 @@ def temp_monthly():
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
 
+# Route to report on the minimum, average, and maximum temperature
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
 
 
