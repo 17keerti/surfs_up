ccf007d8af000b2fb1df918cde3491203156d5d6
Keerti Chaudhary <17keerti@gmail.com>
2022-08-08 00:20:17 -0700

    create route for stations

--- app.py
+++ app.py
@@ -51,3 +51,10 @@ def precipitation():
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
 
+# Station routes
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
